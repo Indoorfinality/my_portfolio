d2f20aa66762a9423dab10fcb04e31039811eb7e
Indoorfinality <anushnachaulagain@gmail.com>
2024-02-13 15:11:36 +0545

    home page start

---
+++ portfolio_website/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HeroImage from "../assets/heroImage.png";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am a Data Scientist</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
