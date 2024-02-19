944554b4c974375a3cc24f62fe765fb7d3740456
Indoorfinality <anushnachaulagain@gmail.com>
2024-02-19 10:41:29 +0545

    completed home page and started sociallinks

--- portfolio_website/src/components/Home.jsx
+++ portfolio_website/src/components/Home.jsx
@@ -4,9 +4,40 @@ import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div>
-      <div>
-        <h2>I am a Data Scientist</h2>
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl text-white">
+            I am a Data Scientist
+          </h2>
+
+          <p className="text-gray-500 py-4 max-w-md">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et saepe
+            ipsum, laboriosam in, doloremque ipsa, esse dignissimos sed nobis
+            inventore qui ut quis dicta blanditiis tot
+          </p>
+          <div>
+            <button
+              className="group text-white
+             w-fit px-6 py-4 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
+              PortFolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </button>
+          </div>
+        </div>
+        <div>
+          <img
+            src={HeroImage}
+            alt="my profile"
+            className="rounded-2xl mx-auto w-2/3 md:w-full"
+          />
+        </div>
       </div>
     </div>
   );
